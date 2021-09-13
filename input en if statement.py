885def9006a169212c40e502980fce7f875dfc4c
Reza <99068159@mydavinci.nl>
2021-09-13 10:28:32 +0200

    Min en Max

--- input en if statement.py
+++ input en if statement.py
@@ -1,11 +1,16 @@
 a = (int(input("Give A a number ")))
 b = (int(input("Give B a number ")))
 
-if a >= b:
-    max = a
+if a > b:
+    Max = a
+    Min = b
     print("A is de grootste waarde")
-elif a <= b:
-    Min = a
+elif a < b:
+    Min = a 
+    Max = b 
     print("A is de kleinste waarde")
 else:
     print("A en B zijn even groot")
+
+print("Het minimum is: " + str(Min))
+print("Het maximum is: " + str(Max))
